c3f4f56110a3408433b2e94a8009821d7adf8ca9
Muhammad Hammad Khan <hjawed716@gmail.com>
2024-10-02 16:53:08 +0500

    2nd

---
+++ class03/src/app/job/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+        <div>
+          
+         <div className="p-6 ">
+        <h5 className="p-4 font-bold" >I am Muhammad Hammad Khan</h5>
+        <p className="ml-14">* I am  currently pursuing an AI and Web 3.0 course under the Governor's Initiative, is dedicated to exploring the evolving landscape of technology. With a focus on innovative solutions through artificial intelligence and decentralized platforms, he is equipped to contribute to the future of digital transformation.</p>
+        </div>
+        </div>
+        
+  );
+}
